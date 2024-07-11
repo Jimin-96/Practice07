02cb4201fdca33fad0edbc00d217cd2118a63abc
jimin <wlalsdl684@naver.com>
2024-07-11 17:12:07 +0900

    .

---
+++ src/com/javaex/ex01/Circle.java
@@ -0,0 +1,30 @@
+package com.javaex.ex01;
+
+public class Circle extends Shape{
+	
+	private int radius;
+	
+	public Circle(String fillColor, int radius){     
+		super(fillColor);
+		this.radius=radius;
+	}
+
+	
+	
+	public void draw(){
+		System.out.println("[원]#면색:" +super.fillColor+ 
+				"  #반지름:" + radius +" 그렸습니다.");                                               
+	}
+
+
+
+	@Override
+	public int getWidth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
+
+
+
